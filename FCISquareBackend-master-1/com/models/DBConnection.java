ec901e77dfccbf8694129843154bdc43ebde3230
20120447 <hadeer_aboqamer@yahoo.com>
2016-03-26 16:34:28 +0200

    Bacend project connect openshift

--- FCISquareBackend-master-1/com/models/DBConnection.java
+++ FCISquareBackend-master-1/com/models/DBConnection.java
@@ -8,9 +8,9 @@ public class DBConnection {
 	private static Connection connection = null;
 
 	public static Connection getActiveConnection() {
-		/*String host = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
+		String host = System.getenv("OPENSHIFT_MYSQL_DB_HOST");
 		String port = System.getenv("OPENSHIFT_MYSQL_DB_PORT");
-		System.out.println(host);*/
+		System.out.println(host);
 		try {
 			Class.forName("com.mysql.jdbc.Driver");
 			connection = DriverManager
